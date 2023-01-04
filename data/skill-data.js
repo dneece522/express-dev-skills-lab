35a3f457b676ab72dddfacf1994c3c83566daf0a
Drew Neece <dneece5@aggienetwork.com>
2023-01-04 17:17:09 -0600

    add fake data directory and .js file to the application

---
+++ data/skill-data.js
@@ -0,0 +1,17 @@
+const skills = [
+  {text: 'Javascript', language: true, unit: 1},
+  {text: 'HTML', language: false, unit: 1},
+  {text: 'CSS', language: false, unit: 1},
+  {text: 'CLI', language: true, unit: 1},
+  {text: 'Node', language: false, unit: 2},
+  {text: 'Python', language: true, unit: 4},
+  {text: 'Express', language: false, unit: 2},
+  {text: 'Mongoose', language: false, unit: 2},
+  {text: 'React', language: true, unit: 3},
+  {text: 'MongoDB', language: false, unit: 2},
+  {text: 'TypeScript', language: true, unit: 4}
+]
+
+export { 
+	skills
+}
