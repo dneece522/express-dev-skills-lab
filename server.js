42baecd0ce1d18c1809d23528c58cb0485e509c8
Drew Neece <dneece5@aggienetwork.com>
2023-01-08 20:23:42 -0600

    add method-override middleware to application

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import express from 'express'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import logger from 'morgan'
+import methodOverride from 'method-override'
 
 // Connect to the database with Mongoose
 import './config/database.js'
@@ -22,12 +23,6 @@ app.set(
 )
 app.set('view engine', 'ejs')
 
-app.use(function(req, res, next) {
-  req.time = new Date().toLocaleTimeString()
-  req.date = new Date().toLocaleDateString()
-  next()
-})
-
 // middleware
 app.use(logger('dev'))
 app.use(express.json())
@@ -37,6 +32,13 @@ app.use(
     path.join(path.dirname(fileURLToPath(import.meta.url)), 'public')
   )
 )
+app.use(methodOverride('_method'))
+
+app.use(function(req, res, next) {
+  req.time = new Date().toLocaleTimeString()
+  req.date = new Date().toLocaleDateString()
+  next()
+})
 
 // mounted routers
 app.use('/', indexRouter)
